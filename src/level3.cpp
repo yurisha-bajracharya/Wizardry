bbae7a608e7bd9bd3c61c72b88e8840d1e758b84
Shruti Maharjan <080bct082.shruti@pcampus.edu.np>
2024-11-08 10:31:08 +0545

    Add empty level3 files

---
+++ src/level3.cpp
@@ -0,0 +1,11 @@
+#include <raylib.h>
+#include "level3.h"
+void InitLevel3(){
+
+}
+void UpdateLevel3(){
+
+}
+void DrawLevel3(){
+    
+} 
---
+++ src/level3.h
@@ -0,0 +1,15 @@
+#ifndef LEVEL3_H
+#define LEVEL3_H
+
+#include "raylib.h"
+
+
+
+
+// Functions
+void InitLevel3();
+void UpdateLevel3();
+void DrawLevel3();  
+
+
+#endif // LEVEL3_H
