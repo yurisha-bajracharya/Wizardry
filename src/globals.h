4198546a77ab0c278e4409446527ebe183bba907
Shruti Maharjan <080bct082.shruti@pcampus.edu.np>
2024-11-08 10:34:20 +0545

    Update globals.h with gamewon and gameover

--- src/globals.h
+++ src/globals.h
@@ -7,4 +7,8 @@
 extern Collectibles collectible;
 extern Character hp;
 extern int CollectibleCount;
+extern int coinsCollected; // Counter for collected coins
+extern bool gameWon; // Flag to track if the game is won
+extern bool gameOver; // Flag to track if the game is over
+
 #endif // GLOBALS_H
