651b3a2cecb29349c49f735bdeec7f1528224e07
Swarnima-Khadka <swarnimakhadka2@gmail.com>
2024-11-11 21:49:34 +0545

    Add files via upload

--- src/menu.h
+++ src/menu.h
@@ -1,7 +1,46 @@
 #ifndef MENU_H
 #define MENU_H
 
-void UpdateMenu();
-void DrawMenu();
+#include<raylib.h>
+#include "button.hpp"
+#include "globals.h"
+
+class Menu {
+private:
+    // Menu assets
+    Texture2D background;
+    Texture2D broom;
+    Texture2D stand;
+    Music theme;
+    //Font font;
+
+    // Buttons
+    Button startButton;
+    Button aboutButton;
+    Button exitButton;
+      Font font = GetFontDefault();
+    // Animation and text variables
+    Vector2 initialPosition;
+    Vector2 targetPosition;
+    Vector2 standPosition;
+
+       Rectangle frameRec, frameRec2, textArea;
+    int frameCounter, currentFrame, frameSpeed;
+    float delayTime, timeElapsed, typingSpeed;
+    bool isBroom, isReachedTarget;
+    const char* messages[6];
+    int messageIndex, charIndex, lineHeight;
+    float timePassed;
+
+    public:
+    Menu();
+    void Init();
+    void UpdateButtons(bool &exit, GameState &currentState);
+    void UpdateAnimation();
+    void UpdateTypingEffect();
+    void Draw();
+    void Unload();
+};
+
 
 #endif // MENU_H
